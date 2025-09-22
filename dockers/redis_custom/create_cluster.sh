8848f12b69626a9bf8cb58707c43563f76debed6
tmkhoa-1812 <tmkhoa-1812@Ubuntu-Desktop-24.04>
2025-09-22 18:20:00 +0700

    feat: add redis cluster (not complete)

---
+++ dockers/redis_custom/create_cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sleep 45
+
+if redis-cli -h redis-node-1 cluster info | grep -q "cluster_state:ok"; then :
+  echo "Cluster already exist"
+else :
+  echo "Creating Redis cluster"
+  yes yes | redis-cli --cluster create \
+    redis-node-1:6379 \
+    redis-node-2:6379 \
+    redis-node-3:6379 \
+    redis-node-4:6379 \
+    redis-node-5:6379 \
+    redis-node-6:6379 \
+    --cluster-replica 1
+fi
+
+exec redis-server /usr/local/etc/redis/redis.conf --appendonly yes
